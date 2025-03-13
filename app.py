b5785d36eb346482c6072dbf217e9ebd37ac8be6
Angel Ouellet <angel@jouellet.net>
2025-03-12 22:11:30 -0400

    Debug=False by default

--- app.py
+++ app.py
@@ -228,4 +228,4 @@ with app.app_context():
         add_user(database, username, password)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
